872ae41e5f0f7951d01b027e465864ca9e022cdd
peeliina <pauliina555@hotmail.fi>
2025-12-15 00:37:49 +0200

    app.js

---
+++ frontend/app.js
@@ -0,0 +1,21 @@
+// Haetaan kaikki eläimet Server A:sta
+fetch("http://localhost:3000/animals")
+  .then(res => res.json())
+  .then(data => {
+    const container = document.getElementById("animals");
+
+    data.forEach(animal => {
+      const card = document.createElement("div");
+      card.className = "card";
+
+      card.innerHTML = `
+        <h3>${animal.name}</h3>
+        <p>${animal.type}, ${animal.age} vuotta</p>
+        <img src="${animal.image || 'https://via.placeholder.com/150'}">
+        <br>
+        <a href="animal.html?id=${animal.id}">Katso lisää</a>
+      `;
+
+      container.appendChild(card);
+    });
+  });
