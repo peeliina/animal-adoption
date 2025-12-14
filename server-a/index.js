2d23f18852c2e3860fac61f19006e16ee7d884da
peeliina <pauliina555@hotmail.fi>
2025-12-15 01:38:06 +0200

    Server-a korjattu

--- server-a/index.js
+++ server-a/index.js
@@ -4,7 +4,15 @@ const fs = require("fs");
 const app = express();
 app.use(express.json());
 
-// Ladataan eläimet tiedostosta
+// CORS – sallitaan frontendin kutsut
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+  next();
+});
+
+// Ladataan eläimet
 let animals = JSON.parse(fs.readFileSync("animals.json"));
 
 // GET /animals
@@ -22,11 +30,10 @@ app.get("/animals/:id", (req, res) => {
 app.post("/animals/:id/adopt", async (req, res) => {
   const animal = animals.find(a => a.id == req.params.id);
 
-  if (animal.status === "adopted") {
+  if (!animal || animal.status === "adopted") {
     return res.status(400).send("Already adopted");
   }
 
-  // Lähetetään Server B:lle
   await fetch("http://server-b:4000/adoptions", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
@@ -39,4 +46,6 @@ app.post("/animals/:id/adopt", async (req, res) => {
   res.sendStatus(200);
 });
 
-app.listen(3000, () => console.log("Server A running"));
+app.listen(3000, () => {
+  console.log("Server A running on port 3000");
+});
