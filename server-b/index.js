fc47b964378d5f5d2f1c572b26a67c094de91476
peeliina <pauliina555@hotmail.fi>
2025-12-15 01:31:46 +0200

    index 2

--- server-b/index.js
+++ server-b/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const fs = require("fs");
-const app = express();
 
+const app = express();
 app.use(express.json());
 
 let adoptions = JSON.parse(fs.readFileSync("adoptions.json"));
