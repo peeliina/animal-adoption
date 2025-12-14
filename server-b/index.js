0f7fd2af08ea36bd814330abb9a2a5e72aa797dc
peeliina <pauliina555@hotmail.fi>
2025-12-15 00:50:30 +0200

    Serverit

---
+++ server-b/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+
+app.use(express.json());
+
+let adoptions = JSON.parse(fs.readFileSync("adoptions.json"));
+
+// POST /adoptions
+app.post("/adoptions", (req, res) => {
+  adoptions.push({
+    animalId: req.body.animalId,
+    date: new Date()
+  });
+
+  fs.writeFileSync("adoptions.json", JSON.stringify(adoptions, null, 2));
+  res.sendStatus(200);
+});
+
+app.listen(4000, () => console.log("Server B running"));
